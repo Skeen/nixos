791c1894a7416668ca4604d9078ff5b6c1ef1463
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2025-11-06 21:53:29 +0100

    Switch neovim for lunarvim and add vim/vi aliases

--- hosts/anvil/default.nix
+++ hosts/anvil/default.nix
@@ -113,8 +113,8 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-neovim
 git
+(lunarvim.override {viAlias = true; vimAlias = true; nvimAlias = true;})
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
