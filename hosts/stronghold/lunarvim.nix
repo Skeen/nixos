b99ec5e23521e63eff834720cc0010f67bef90a0
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2025-12-17 22:16:30 +0100

    feat(stronghold): cache lunarvim cache files

--- hosts/stronghold/lunarvim.nix
+++ hosts/stronghold/lunarvim.nix
@@ -7,4 +7,22 @@
     })
   ];
   environment.variables.EDITOR = "lvim";
+
+  # These files are just cache which can be removed whenever
+  environment.persistence."/nix/cache" = {
+    hideMounts = true;
+    users.emil = {
+      directories = [
+        ".cache/lvim/"
+        ".local/share/lvim"
+        ".local/state/lvim"
+      ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "e     /nix/cache/home/emil/.cache/lvim - - - 14d -"
+    "e     /nix/cache/home/emil/.local/share/lvim - - - 14d -"
+    "e     /nix/cache/home/emil/.local/state/lvim - - - 14d -"
+  ];
 }
