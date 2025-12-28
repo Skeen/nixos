81f51c7d17c19eee7dcc653b7066eababd618f84
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2025-12-28 11:25:14 +0100

    feat(hearth, xfce): added TODOs about xfconfs to declare

--- hosts/hearth/xfce/default.nix
+++ hosts/hearth/xfce/default.nix
@@ -1,4 +1,16 @@
 {...}: {
+  # TODO: Ensure that numpad is always enabled on login
+  #       xfconf-query -c keyboards -p / -v -l
+  # TODO: Declare Thunar configuration
+  #       xfconf-query -c thunar -p / -v -l
+  # TODO: Declare xfce4-keyboard-shortcuts:
+  #       xfconf-query -c xfce4-keyboard-shortcuts -p / -v -l
+  # TODO: Declare xfwm4:
+  #       xfconf-query -c xfwm4 -p / -v -l
+  # TODO: Declare xfce4-session:
+  #       xfconf-query -c xfce4-session -p / -v -l
+  #       "general/SaveOnExit" = false;
+
   imports = [
     ./panel.nix
     ./background.nix
